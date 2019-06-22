ac91cd60f70cdf3b47fb710512ac3fbe5d1dc9ee
Blake Motl <blake@motl.dev>
2019-06-21 23:06:13 -0700

    Sorry to make one large commit. Kind of went ham. Switched ACL to a function instead of a long if statement. Revamped command processing. It now is a series od switch statements that call other functions. Right now it can only do a nickname vote though. Added a goroutine to process the vote asyncronously. Added a lot of error handling with messages. Bot can now react to yes votes. Time of the vote and number of votes needed(threshold) are both configurable. Added a function to get an emojiID by name that i didn't end up needing but I'm leaving it.

--- main.go
+++ main.go
@@ -9,12 +9,19 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 var tokenFile = "bot.key"
 var usernameToId map[string]string
+var defaultVoteTime, _ = time.ParseDuration("20s")
+var threshold int
+var voters []string
+var activeVote = false
+
+const invalidCommand = "I'm afraid I can't do that...(yet)"
 
 func main() {
 	token := readKeyFile()
@@ -56,35 +63,117 @@ func readKeyFile() string {
 	return string(tokenSlice)
 }
 
+func strInSlice(str string, slice []string) bool {
+	for _, v := range slice {
+		if strings.Compare(v, str) == 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func inACL(user string) bool {
+	switch user {
+	case
+		"QuantumQuip",
+		"picthebear",
+		"MetalKnuckles":
+		return true
+	}
+	return false
+}
+
+func getEmojiId(name string, guild *discordgo.Guild) string {
+	for _, v := range guild.Emojis {
+		if strings.Compare(v.Name, name) == 0 {
+			return v.ID
+		}
+	}
+	return ""
+}
+
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the autenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
+	//g, _ := s.Guild(m.GuildID)
+	//getEmojiId("thumbsup", g)
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-	commands := strings.Split(m.Content, " ")
-
-	if strings.Compare(commands[0], "!vote") != 0 {
+	if m.Content[0] == '!' {
+		handleCommand(s, m, m.Content[1:])
+	} else {
 		return
 	}
+}
 
-	if m.Author.Username != "QuantumQuip" && m.Author.Username != "picthebear" && m.Author.Username != "MetalKnux" {
+func handleCommand(s *discordgo.Session, m *discordgo.MessageCreate, command string) {
+	commands := strings.Split(command, " ")
+	if !inACL(m.Author.Username) {
 		s.ChannelMessageSend(m.ChannelID, "You have no power here!")
 		return
 	}
-
-	ID, err := getUserIDfromString(s, m.GuildID, commands[1])
-
-	if err != nil {
-		s.ChannelMessageSend(m.ChannelID, err.Error())
-		log.Fatal(err)
+	switch commands[0] {
+	case "vote":
+		vote(s, m, commands[1:])
+	case "test":
+		s.ChannelMessageSend(m.ChannelID, "Testing asyncronous calls(go routines)...")
+	default:
+		s.ChannelMessageSend(m.ChannelID, "I'm afraid I can't do that...(yet)")
 	}
-	s.ChannelMessageSend(m.ChannelID, "The people have spoken!")
-	s.GuildMemberNickname(m.GuildID, ID, commands[2])
+}
+
+func vote(s *discordgo.Session, m *discordgo.MessageCreate, commands []string) {
+	if len(commands) < 1 {
+		s.ChannelMessageSend(m.ChannelID, "Vote command format is !vote action")
+		return
+	}
+	switch commands[0] {
+	case "create":
+		threshold = 1
+		go createVote(s, m, commands[1:])
+	case "yes":
+		if activeVote {
+			if !strInSlice(m.Author.Username, voters) {
+				voters = append(voters, m.Author.Username)
+				threshold--
+				s.MessageReactionAdd(m.ChannelID, m.Message.ID, "👍")
+			} else {
+				s.ChannelMessageSend(m.ChannelID, "You may only vote once per vote!")
+			}
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "There is no active vote! Start one with !vote create.")
+		}
+	default:
+		s.ChannelMessageSend(m.ChannelID, "I'm afraid I can't do that...(yet)")
+	}
+}
+
+func createVote(s *discordgo.Session, m *discordgo.MessageCreate, commands []string) {
+	if len(commands) < 2 {
+		s.ChannelMessageSend(m.ChannelID, "Vote create command format is !vote create username nickname")
+		return
+	}
+	activeVote = true
+	voters = nil
+	ID, err := getUserIDfromString(s, m.GuildID, commands[0])
+	if err != nil {
+		s.ChannelMessageSend(m.ChannelID, "That user doesn't appear to exist!")
+		return
+	}
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("A vote has started to change %v's nickname to %v!", commands[0], commands[1]))
+	time.Sleep(defaultVoteTime)
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("A vote has ended after %v!", defaultVoteTime))
+	if threshold == 0 {
+		s.ChannelMessageSend(m.ChannelID, "The people have spoken!")
+		s.GuildMemberNickname(m.GuildID, ID, commands[1])
+	} else {
+		s.ChannelMessageSend(m.ChannelID, "The vote has failed!")
+	}
+	activeVote = false
 }
 
 func getUserIDfromString(s *discordgo.Session, guildID string, user string) (string, error) {
